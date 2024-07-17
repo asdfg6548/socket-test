1c291ad8001b5045605149604dc4485854a78110
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-07-17 21:33:44 +0900

    소켓 기본 연결 확인

---
+++ src/test/java/ac/su/ctctct/CtctctApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.ctctct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtctctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
